86a6f3ab97677cafb9e92171caa9e5a05c11152f
bohdan <shovkopliasbohdan@gmail.com>
2025-03-21 21:54:25 +0100

    chore: init project; tailwind theme setup

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+function Button() {
+   return <button></button>;
+}
+
+Button.Accent = function () {
+   return <button></button>;
+};
+
+Button.Absolute = function () {
+   return <button></button>;
+};
+
+export { Button };
